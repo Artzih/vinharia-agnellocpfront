983dabce17285ccbb2cf962b8fd1604496a838cb
Arthur Kazuo B K <arthur@ladio.com.br>
2026-04-06 10:40:21 -0300

    adicionando informações coletadas no console

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,12 @@ let nome = prompt("Digite o nome do vinho:");
 let tipo = prompt("Digite o tipo do vinho (Tinto, Branco, ou Rosé)");
 
 
-//Aviso para ver no console alert()dsfds
+//Aviso para ver no console alert()
 
 alert("Cadastro realizado! Veja os detalhes no console");
 
+//Informações coletadas console.log()
+console.log("Cadastro do Vinho");
+console.log("-----------------------------");
+console.log("Nome:", nome);
+console.log("Tipo:", tipo);
